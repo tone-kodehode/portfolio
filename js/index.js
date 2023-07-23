b89ede47413e2a2677e818111d1494953aa77f93
Tone <tone.kodehode@gmail.com>
2023-07-23 18:57:00 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-const currentPage = window.location.href;
-const navLinks = document.querySelectorAll('.nav-link');
-
-console.log(navLinks);
-
-navLinks.forEach(link => {
-    if (link.href === currentPage) {
-        link.classList.add('active-link'); 
-    } else {
-        link.classList.remove('active-link'); 
-    }
-});
 
