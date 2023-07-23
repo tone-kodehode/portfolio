74b742b6aed223de88fd45d575b82adf3b4a0ce1
Tone <tone.kodehode@gmail.com>
2023-07-23 22:00:51 +0200

    active Nav Link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
+const navLink = document.querySelectorAll('.nav-link')
+const windowPathname = window.location.pathname;
+
+navLink.forEach(navLink => {
+    const navLinkPathname = URL(navLink.href).pathname;
+
+    if ((windowPathname === navLinkPathname) || (windowPathname === '#index.html' && "#")) {
+        navLink.classList.add('active')
+    }
+})
+
 
