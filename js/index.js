cf26dbfd4c60124fa28f8433050c3ce67f0cad99
Tone <tone.kodehode@gmail.com>
2023-07-23 22:05:28 +0200

    new URL update

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ const navLink = document.querySelectorAll('.nav-link')
 const windowPathname = window.location.pathname;
 
 navLink.forEach(navLink => {
-    const navLinkPathname = URL(navLink.href).pathname;
+    const navLinkPathname = new URL(navLink.href).pathname;
 
-    if ((windowPathname === navLinkPathname) || (windowPathname === '#index.html' && "#")) {
+    if ((windowPathname === navLinkPathname) || (windowPathname === '/index.html' && navLink.getAttribute('href') === "#")) {
         navLink.classList.add('active')
     }
 })
