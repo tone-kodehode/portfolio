1c3f18aa868bdc3814fc4abe87e9c8391603ff46
Tone <tone.kodehode@gmail.com>
2023-07-23 22:09:46 +0200

    error url

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-const navLink = document.querySelectorAll('.nav-link')
+const navLink = document.querySelectorAll('.nav-link');
 const windowPathname = window.location.pathname;
 
 navLink.forEach(navLink => {
-    const navLinkPathname = new URL(navLink.href).pathname;
+    if (navLink.href) {
+        try {
+            const navLinkPathname = new URL(navLink.href).pathname;
 
-    if ((windowPathname === navLinkPathname) || (windowPathname === '/index.html' && navLink.getAttribute('href') === "#")) {
-        navLink.classList.add('active')
+            if ((windowPathname === navLinkPathname) || (windowPathname === '/index.html' && navLink.getAttribute('href') === "#")) {
+                navLink.classList.add('active');
+            }
+        } catch (error) {
+
+            console.error(`Invalid URL: ${navLink.href}`);
+        }
     }
-})
+});
 
 
