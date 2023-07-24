2b8242418fda25d3b5752acceb393137ff0e9428
Tone <tone.kodehode@gmail.com>
2023-07-24 16:48:24 +0200

    url activ test

--- js/index.js
+++ js/index.js
@@ -1,40 +1,57 @@
+// const navLink = document.querySelectorAll('.nav-link');
+// const windowPathname = window.location.pathname;
+
+// navLink.forEach(navLink => {
+//     if (navLink.href) {
+//         if (navLink.href.startsWith("mailto:")) {
+           
+//             return;
+//         }
+
+//         if (navLink.getAttribute('href') === "#") {
+            
+//             if (windowPathname === '/index.html' && window.location.hash === "") {
+//                 navLink.classList.add('active');
+//                 console.log(navLink, 'active');
+//             } else {
+//                 navLink.classList.remove('active');
+//                 console.log(navLink, 'remove');
+//             }
+//         } else {
+           
+//             try {
+//                 const navLinkPathname = new URL(navLink.href).pathname;
+
+//                 if (windowPathname === navLinkPathname) {
+//                     navLink.classList.add('active');
+//                     console.log(navLink, 'active');
+//                 } else {
+//                     navLink.classList.remove('active');
+//                     console.log(navLink, 'remove');
+//                 }
+//             } catch (error) {
+//                 console.error(`Invalid URL: ${navLink.href}`);
+//             }
+//         }
+//     }
+// });
+
 const navLink = document.querySelectorAll('.nav-link');
 const windowPathname = window.location.pathname;
 
-navLink.forEach(navLink => {
-    if (navLink.href) {
-        if (navLink.href.startsWith("mailto:")) {
-           
-            return;
-        }
-
-        if (navLink.getAttribute('href') === "#") {
-            
-            if (windowPathname === '/index.html' && window.location.hash === "") {
-                navLink.classList.add('active');
-                console.log(navLink, 'active');
-            } else {
-                navLink.classList.remove('active');
-                console.log(navLink, 'remove');
-            }
-        } else {
-           
-            try {
-                const navLinkPathname = new URL(navLink.href).pathname;
-
-                if (windowPathname === navLinkPathname) {
+function markActiveLink(){
+    navLink.forEach(navLink => {
+        const a = window.location.href.includes(navLink.href)
+            console.log(a, window.location.fragment)
+                if (a) {
                     navLink.classList.add('active');
                     console.log(navLink, 'active');
                 } else {
                     navLink.classList.remove('active');
                     console.log(navLink, 'remove');
                 }
-            } catch (error) {
-                console.error(`Invalid URL: ${navLink.href}`);
-            }
-        }
-    }
-});
+    });
+}
 
 
 
