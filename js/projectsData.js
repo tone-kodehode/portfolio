1ed9060deb28979a5ad59a7d59c48663866f7274
Tone <tone.kodehode@gmail.com>
2023-09-01 13:36:37 +0200

    Page layout

--- js/projectsData.js
+++ js/projectsData.js
@@ -1,6 +1,6 @@
 const projectsData = [
     {
-      title: "Flowers & Garden",
+      title: "React shop",
       imgSrc: "img/Skjermbilde 2023-07-29 kl. 17.33.27.png",
       description: "Shop project in progress. Working with reusable component in React, and avoid repeating code gave a practical approach towards scalable code implementation.",
       githubLink: "https://github.com/",
@@ -8,21 +8,30 @@ const projectsData = [
       icons: ["fa-react", "fa-js", "fa-html5", "fa-css3", "fa-figma"],
     },
     {
-      title: "JS Piano",
+      title: "User interaction",
       imgSrc: "img/Skjermbilde 2023-08-11 kl. 16.27.55.png",
       description: "A web app created in JavaScript to illustrates the flexibility that JavaScript affords. Where clickable buttons play specific sound on click and keypress as a response to user interaction.",
       githubLink: "https://github.com/tone-kodehode/jsPiano.git",
       liveViewLink: "https://tone-kodehode.github.io/jsPiano/",
       icons: ["fa-js", "fa-css3"],
     },
-    {
-      title: "Note App",
-      imgSrc: "img/Skjermbilde 2023-08-11 kl. 17.44.05.png",
-      description: "Note app that stores data in the browser with the use of localStorage. Data from user continues being stored in the browser after the browser is closed.",
-      githubLink: "https://github.com/tone-kodehode/note-app.git",
-      liveViewLink: " https://tone-kodehode.github.io/note-app/",
-      icons: ["fa-js", "fa-html5", "fa-css3"],
-    },
+    // {
+    //   title: "LocalStorage",
+    //   imgSrc: "img/Skjermbilde 2023-08-11 kl. 17.44.05.png",
+    //   description: "Note app that stores data in the browser with the use of localStorage. Data from user continues being stored in the browser after the browser is closed.",
+    //   githubLink: "https://github.com/tone-kodehode/note-app.git",
+    //   liveViewLink: " https://tone-kodehode.github.io/note-app/",
+    //   icons: ["fa-js", "fa-html5", "fa-css3"],
+    // },
+    // {
+    //   title: "Parallax",
+    //   imgSrc: "img/Skjermbilde 2023-08-31 kl. 12.31.29.png",
+    //   description: "Simple Parallax scrolling story where the background image moves at a different speed than the foreground content as you scroll.",
+    //   githubLink: "https://github.com/tone-kodehode/task3-apple",
+    //   liveViewLink: "https://tone-kodehode.github.io/task3-apple/",
+    //   icons: ["fa-html5", "fa-css3"],
+    // },
+
 
   ];
   
